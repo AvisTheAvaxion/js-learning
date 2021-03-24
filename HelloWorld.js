880ffdbbf3912b6a4baad7f507e02c63639813a9
AvisTheAvaxion <legodaniele@gmail.com>
2021-03-24 16:48:04 -0400

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+let num1 = 5
+let num2 = 3
+console.log(num1 + num2)
+
+function add(number1, number2)
+{
+	let sum = number1 + number2
+	return sum
+}
+console.log(add(20,32))
